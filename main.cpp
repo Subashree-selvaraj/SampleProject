46b7e41e4586d0ec6c5c3ad08044d9c08862a828
Subashree-selvaraj <subashreevjc@gmail.com>
2026-09-15 11:16:37 +0530

    feature/power: add power() method with cout output

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@ public:
     float add(float a, float b) { return a + b; }
     float subtract(float a, float b) { return a - b; }
     float multiply(float a, float b) { return a * b; }
+    float power(float base, int exp) {
+    float result = 1;
+    for (int i = 0; i < exp; i++) result *= base;
+    return result;
+}
     
     
     float divide(float a, float b) {
@@ -27,6 +32,7 @@ int main() {
     cout << "Sum: " << calc.add(a, b) << endl;
     cout << "Difference: " << calc.subtract(a, b) << endl;
     cout << "Product: " << calc.multiply(a, b) << endl;
+    cout << "Power: " << calc.power(a, (int)b) << endl;
     cout << "Quotient: " << calc.divide(a, b) << endl;
     
     return 0;
