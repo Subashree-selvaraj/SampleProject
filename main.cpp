5d83e609a8784b6a70ddb7b563b401f514574292
Subashree-selvaraj <subashreevjc@gmail.com>
2026-09-15 10:17:43 +0530

    Add basic OOP Calculator class with arithmetic operations

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 using namespace std;
 
+class Calculator {
+public:
+    float add(float a, float b) { return a + b; }
+    float subtract(float a, float b) { return a - b; }
+    float multiply(float a, float b) { return a * b; }
+    
+    
+    float divide(float a, float b) {
+        if (b == 0) {
+            cout << "Error: Division by zero! ";
+            return 0;
+        }
+        return a / b;
+    }
+};
+
 int main() {
-    cout << "Hello!" << endl;
+    Calculator calc;
+    float a, b;
+    
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    
+    cout << "Sum: " << calc.add(a, b) << endl;
+    cout << "Difference: " << calc.subtract(a, b) << endl;
+    cout << "Product: " << calc.multiply(a, b) << endl;
+    cout << "Quotient: " << calc.divide(a, b) << endl;
+    
     return 0;
 }
